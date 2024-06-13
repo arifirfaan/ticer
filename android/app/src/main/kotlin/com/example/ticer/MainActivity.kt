8de78da4915a9a6e0735b343d4158dcb61722dab
arifirfaan <arifirfaanworks@gmail.com>
2024-06-13 12:01:09 +0800

    first commit

---
+++ android/app/src/main/kotlin/com/example/ticer/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.ticer
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/dashboard_page.dart
@@ -0,0 +1,138 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+class DashboardPage extends StatefulWidget {
+  const DashboardPage({super.key});
+
+  @override
+  State<DashboardPage> createState() => _DashboardPageState();
+}
+
+class _DashboardPageState extends State<DashboardPage> {
+
+  Widget listAddedWidget =  const Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Text(
+                                      "Machines Sdn Bhd",
+                                      style: TextStyle(
+                                        fontSize: 14,
+                                        fontWeight: FontWeight.w500,
+                                      )
+                                    ),
+                                    Text(
+                                      "DD / MM / YY",
+                                      style: TextStyle(
+                                        fontSize: 12,
+                                        fontWeight: FontWeight.w400,
+                                      )
+                                    )
+                                  ],
+                                ),
+                                    Text(
+                                      "RM 00.00",
+                                      style: TextStyle(
+                                          fontSize: 14,
+                                          fontWeight: FontWeight.w600,
+                                      )
+                                  )
+                                  ],
+                              );
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.all(16),
+      child: ListView(
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text('Welcome', style: TextStyle(color: Color(0xFF9D9D9D)),),
+                  Text('Muhammad', style: TextStyle(fontSize: 32, fontWeight: FontWeight.w600),), // Dynamic Name
+                ],
+              ),
+              ClipOval(
+                child: Image.network(
+                  'https://thumbs.dreamstime.com/b/businessman-icon-vector-male-avatar-profile-image-profile-businessman-icon-vector-male-avatar-profile-image-182095609.jpg', // Use your image URL here
+                  width: 56, // Adjust the width as needed
+                  height: 56, // Adjust the height as needed
+                  fit: BoxFit.cover,
+                ),
+              ),
+            ],
+          ),
+          Container(
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(8 ),
+              color: const Color(0xFF333333),
+            ),
+            padding: const EdgeInsets.all(12),
+            child: const Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text('You have spent this month', style: TextStyle(fontSize: 16, color: Colors.white)),
+                Text('RM 0.00', style: TextStyle(fontSize: 45, fontWeight: FontWeight.w700, color: Colors.white))
+              ],
+            ),
+          ),
+          const SizedBox(height: 20,),
+          const Padding(
+            padding: EdgeInsets.all(8.0),
+            child: Text(
+              "Last Receipt Added",
+              style: TextStyle(
+                  fontSize: 14,
+                  fontWeight: FontWeight.w500,
+              )
+            ),
+          ),
+          Container(
+            padding: const EdgeInsets.all(12),
+            decoration: BoxDecoration(
+              color: const Color(0xFFF1F1F1),
+              borderRadius: BorderRadius.circular(4)
+            ),
+            child:listAddedWidget
+          ),
+          const SizedBox(height: 8,),
+          Container(
+            padding: const EdgeInsets.all(12),
+            decoration: BoxDecoration(
+              color: const Color(0xFFF1F1F1),
+              borderRadius: BorderRadius.circular(4)
+            ),
+            child:listAddedWidget
+          ),
+          Container(
+            decoration: BoxDecoration(
+              border: Border.all(color: Colors.black38,width: 2),
+              borderRadius: BorderRadius.circular(12)
+            ),
+            child: Row(
+              children:[
+                //SvgPicture.asset('svg/dashboard_vector.svg'),
+                const Column(children: [
+                  Text(
+                    "Reports", style: TextStyle(fontSize: 15,fontWeight: FontWeight.w600)
+                  ),
+                  Text(
+                    "You can view expenses reports in categories",
+                    style: TextStyle(fontSize: 12 ,fontWeight: FontWeight.w400)
+                  )
+                ],)
+              ]
+            )
+          )
+        ],
+      ),
+    );
+  }
+}
