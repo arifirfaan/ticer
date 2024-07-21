16f45397c05b8ed33f8f7115a179c952dfbf2565
arifirfaan <arifirfaanworks@gmail.com>
2024-07-22 02:02:13 +0800

    update

--- lib/profile_page.dart
+++ lib/profile_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:ticer/components/bottom_bar.dart';
+import 'package:flutter/cupertino.dart';
 
 class ProfilePage extends StatefulWidget {
   const ProfilePage({super.key});
@@ -9,6 +10,30 @@ class ProfilePage extends StatefulWidget {
 }
 
 class _ProfilePageState extends State<ProfilePage> {
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  Widget submenu({required String title, required Function onpressed}) {
+    return Container(
+      margin: EdgeInsets.symmetric(vertical: 4),
+      child: InkWell(
+        onTap: onpressed(),
+        child: Container(
+          padding: EdgeInsets.all(10),
+          decoration: BoxDecoration(
+              border: Border.all(color: Colors.black),
+              borderRadius: BorderRadius.circular(10)),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [Text(title), Icon(Icons.navigate_next)],
+          ),
+        ),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -21,6 +46,85 @@ class _ProfilePageState extends State<ProfilePage> {
                 Text(
                   'Profile',
                   style: TextStyle(fontSize: 32, fontWeight: FontWeight.w600),
+                ),
+                Container(
+                  margin: EdgeInsets.symmetric(vertical: 25),
+                  padding: EdgeInsets.all(12),
+                  decoration: BoxDecoration(
+                      border: Border.all(color: Colors.black),
+                      borderRadius: BorderRadius.circular(12)),
+                  child: Row(
+                    children: [
+                      ClipOval(
+                        child: Image.network(
+                          'https://thumbs.dreamstime.com/b/businessman-icon-vector-male-avatar-profile-image-profile-businessman-icon-vector-male-avatar-profile-image-182095609.jpg', // Use your image URL here
+                          width: 56, // Adjust the width as needed
+                          height: 56, // Adjust the height as needed
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                      SizedBox(
+                        width: 14,
+                      ),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            "Muhammad bin Ahmad",
+                            style: TextStyle(
+                                fontSize: 17, fontWeight: FontWeight.w600),
+                          ),
+                          Text(
+                            "muhammadfirdaus.work@gmail.com",
+                            style: TextStyle(fontSize: 13),
+                          )
+                        ],
+                      )
+                    ],
+                  ),
+                ),
+                submenu(title: "Email Address", onpressed: () {}),
+                submenu(title: "Phone Number", onpressed: () {}),
+                submenu(title: "Upgrade to Pro", onpressed: () {}),
+                submenu(title: "Terms and Conditions", onpressed: () {}),
+                submenu(title: "Privacy Policy", onpressed: () {}),
+                submenu(title: "Backup Data", onpressed: () {}),
+                submenu(title: "Clear Data", onpressed: () {}),
+                InkWell(
+                  onTap: () {
+                    showCupertinoDialog(
+                      context: context,
+                      builder: (BuildContext context) => CupertinoAlertDialog(
+                        title: Text('Logout'),
+                        content: Text('Are you sure to logout?'),
+                        actions: <Widget>[
+                          CupertinoDialogAction(
+                            child: Text('Yes'),
+                            onPressed: () {
+                              Navigator.of(context).pop(); // Close the dialog
+                            },
+                          ),
+                          CupertinoDialogAction(
+                            child: Text('No'),
+                            onPressed: () {
+                              Navigator.of(context).pop(); // Close the dialog
+                            },
+                          ),
+                        ],
+                      ),
+                    );
+                  },
+                  child: Container(
+                      margin: EdgeInsets.symmetric(vertical: 4),
+                      padding: EdgeInsets.all(10),
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(8),
+                          color: Colors.red),
+                      child: Center(
+                          child: Text(
+                        "Logout",
+                        style: TextStyle(color: Colors.white),
+                      ))),
                 )
               ],
             ),
--- lib/receipt_page.dart
+++ lib/receipt_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:ticer/components/bottom_bar.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:cupertino_icons/cupertino_icons.dart';
 
 class ReceiptPage extends StatefulWidget {
   const ReceiptPage({super.key});
@@ -18,10 +20,70 @@ class _ReceiptPageState extends State<ReceiptPage> {
             padding: const EdgeInsets.all(16),
             child: ListView(
               children: [
-                Text(
-                  'Receipt',
-                  style: TextStyle(fontSize: 32, fontWeight: FontWeight.w600),
-                )
+                Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        'Receipt',
+                        style: TextStyle(
+                            fontSize: 32, fontWeight: FontWeight.w600),
+                      ),
+                      InkWell(
+                        onTap: () {
+                          showCupertinoDialog(
+                            context: context,
+                            builder: (BuildContext context) =>
+                                CupertinoAlertDialog(
+                              title: Text('Add New Receipt'),
+                              content:
+                                  Text('The feature is not ready yet Firdaus.'),
+                              actions: <Widget>[
+                                CupertinoDialogAction(
+                                  child: Text('Yes'),
+                                  onPressed: () {
+                                    Navigator.of(context)
+                                        .pop(); // Close the dialog
+                                  },
+                                ),
+                                CupertinoDialogAction(
+                                  child: Text('No'),
+                                  onPressed: () {
+                                    Navigator.of(context)
+                                        .pop(); // Close the dialog
+                                  },
+                                ),
+                              ],
+                            ),
+                          );
+                        },
+                        child: Container(
+                          width: 30,
+                          height: 30,
+                          decoration: BoxDecoration(
+                              color: Color(0xFFCEFA01),
+                              borderRadius: BorderRadius.circular(20)),
+                          child: Center(child: Icon(Icons.add)),
+                        ),
+                      ),
+                    ]),
+                SizedBox(
+                  height: 15,
+                ),
+                Container(
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10.0),
+                    color: Colors.grey[200], // Background color
+                  ),
+                  child: TextField(
+                      decoration: InputDecoration(
+                          contentPadding: EdgeInsets.symmetric(
+                              horizontal: 16.0, vertical: 12),
+                          hintText: 'Search',
+                          hintStyle: TextStyle(fontWeight: FontWeight.w300),
+                          border: InputBorder.none, // Remove the default border
+                          suffixIcon: Icon(CupertinoIcons.mic_fill),
+                          prefixIcon: Icon(CupertinoIcons.search))),
+                ),
               ],
             ),
           ),
